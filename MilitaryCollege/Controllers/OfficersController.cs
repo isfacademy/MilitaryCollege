b2c82b117e4aae81307cf862a8ffb58c8725f639
isfacademy <isfacademy.2023@gmail.com>
2021-02-18 14:47:15 +0200

    c

--- MilitaryCollege/Controllers/OfficersController.cs
+++ MilitaryCollege/Controllers/OfficersController.cs
@@ -552,6 +552,7 @@ namespace MilitaryCollege.Controllers
             var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             // tournament Id assigned to current user Id 
             int? TournamentId = _context.UserTournaments.Where(u => u.UserId == userId).Select(t => t.TournamentId).FirstOrDefault();
+            //ToDo:
             var officers = _context.Officers.Where(i => i.TournamentId == TournamentId).OrderBy(m => m.MilitaryNumber).ToList();
             return View(officers);
         }
